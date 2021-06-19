208e13aef4a05d6b9aee85205459e13496845bd4
Knpranav <darklight@gmail.com>
2021-06-19 19:06:32 +0530

    yay!

---
+++ drum.js
@@ -0,0 +1,72 @@
+
+var numberOfButtons = document.querySelectorAll(".button").length;
+  
+for (var i = 0; i < numberOfButtons; i++) 
+{
+    document.querySelectorAll(".button")[i].addEventListener("click", function() 
+    {
+      var buttonStyle = this.innerHTML;
+      sound(buttonStyle);
+      animation(buttonStyle);
+  });
+}
+
+document.addEventListener("keypress", function(event) {
+    sound(event.key);
+    animation(event.key);
+  });
+
+  function sound(key) {
+    switch (key) {
+      case "a":
+        var sound1 = new Audio("sounds/boom.wav");
+        sound1.play();
+        break;
+    
+      case "s":
+        var sound2 = new Audio("sounds/clap.wav");
+        sound2.play();
+        break;
+    
+      case "d":
+        var sound3 = new Audio("sounds/tom.wav");
+        sound3.play();
+        break;
+    
+      case "f":
+        var sound4 = new Audio("sounds/kick.wav");
+        sound4.play();
+        break;
+    
+      case "g":
+        var sound5 = new Audio("sounds/snare.wav");
+        sound5.play();
+        break;
+    
+      case "h":
+        var sound6 = new Audio("sounds/ride.wav");
+        sound6.play();
+        break;
+    
+      case "j":
+        var sound7 = new Audio("sounds/crash.wav");
+        sound7.play();
+        break;
+
+        case "k":
+            var sound8 = new Audio("sounds/hihat.wav");
+            sound8.play();
+            break;
+    
+      default: console.log(key);
+    }
+  }
+
+  function animation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("animation");
+    
+    setTimeout(function() {
+      activeButton.classList.remove("animation");
+    }, 150);
+  }
